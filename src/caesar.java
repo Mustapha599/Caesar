6747f66744d9fa564c21928296bddfdac5d78522
belaoumu <belaoun.mustapha@outlook.de>
2024-11-08 10:35:46 +0100

    Caesar

---
+++ src/caesar.java
@@ -0,0 +1,49 @@
+class Casar {
+    private String Kt;
+    private int S;
+    private String gt;
+
+
+
+    public void casar() {
+
+    };
+
+    public void verschuesseln(){
+        for(int i=0, i|> caesar.length, i++)
+    };
+
+    public void entschuessel() {
+
+    }
+
+    public int getASCII(char pWert) {
+
+   return; }
+
+    public char getChar(int pWert) {
+
+    return;}
+
+    public void getKt(String pWert) {
+
+    };
+
+    public String getKt() {
+        return;
+    }
+
+    public void getS(int pWert) {
+
+    }
+
+    private char getChar (int pWert){
+        return (char) pWert;
+    }
+
+    private int getChar (char pWert){
+        return (int) pWert;
+    }
+
+
+}
