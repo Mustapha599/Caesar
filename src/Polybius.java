3abf31a24b268bfaf57a91c393685534e930b44b
belaoumu <belaoun.mustapha@outlook.de>
2024-12-03 13:22:22 +0100

    h

--- src/Polybius.java
+++ src/Polybius.java
@@ -15,7 +15,7 @@ private void bfmS(){
         //Durchläuft die Spalte
         for (int j = 0; j < alphabetQuadrat.length-1; j++) {
             alphabetQuadrat[i][j]=A;
-
+        //Falls Stelle A gleich I dann erhöhe um 1. Dann haben wir J
             if(A==73) {
                 A++;
             }
@@ -27,7 +27,12 @@ private void bfmS(){
 
 }
 public void verschluesseln(){
-
+gt="";
+    for (int i = 0; i < kt.length() ; i++) {
+        for (int j = 0; j <kt.length() ; j++) {
+            gt=kt+i+j;
+        }
+    }
 
 
 
